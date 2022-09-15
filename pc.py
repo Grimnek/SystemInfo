09b400bf5dfc5d999581681d67f7cbde0bf4d408
AlexTheProgrammer <72882565+AlexProgramep@users.noreply.github.com>
2022-09-15 15:57:38 +0300

    Update pc.py

--- pc.py
+++ pc.py
@@ -1,16 +1,16 @@
 import platform, sys, cpuinfo
 
-pc_info = f"""О компьютере:\n
-Сетевое имя ПК:              {platform.node()}
-Процессор:                       {cpuinfo.get_cpu_info()['brand_raw']}
-Имя:                                    {platform.system()}
-Выпуск:                              {platform.release()}
-Версия:                               {platform.version()}
-Архитектура битов:          {platform.architecture()[0]}"""
+pc_info = f"""Про комп'ютер:\n
+Мережеве ім'я ПК:              {platform.node()}
+Процесор:                       {cpuinfo.get_cpu_info()['brand_raw']}
+Ім'я:                                    {platform.system()}
+Випуск:                              {platform.release()}
+Версія:                               {platform.version()}
+Архітектура бітів:          {platform.architecture()[0]}"""
 
 
 python_info = f"""О Python:\n
-Дата сборки:                               {platform.python_build()[1]}
-Имплементация Python:          {platform.python_implementation()}
-Версия Python:                           {platform.python_version()}
-Путь к Python:                            {sys.executable}"""
+Дата складання:                               {platform.python_build()[1]}
+Імплементація Python:          {platform.python_implementation()}
+Версія Python:                           {platform.python_version()}
+Шлях до Python:                            {sys.executable}"""
